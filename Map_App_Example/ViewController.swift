f835b980e8ed98593ec55c37f25c81a2b05f42d9
koseali <kose.ali@std.izu.edu.tr>
2020-11-06 17:52:39 +0300

    want  to user location permission and get to user location

--- Map_App_Example/ViewController.swift
+++ Map_App_Example/ViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController , MKMapViewDelegate{
+import CoreLocation
+
+class ViewController: UIViewController , MKMapViewDelegate,CLLocationManagerDelegate{
 
     // First Step: import mapkit library and delegate MKMapViewDelegate  write didload mapview.delegate and you will see to map on your app view
+    var locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // but spend a lot of battery for best location
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
